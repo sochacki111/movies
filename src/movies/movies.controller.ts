b72f2e41e6b367bd456d2092580fadbf96fc45e7
Michał Sochacki <michalsochacki111@gmail.com>
2022-10-27 11:56:13 +0200

    Feat/get movies (#1)
    * feat: get movies
    
    * feat: get movies, query params
    
    * feat: filter movies by genres
    
    * refactor: filter movies by genres
    
    Co-authored-by: sochacki111 <michal.sochacki111@gmail.com>

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
+import type { Response } from 'express';
+import { createReadStream } from 'fs';
+import JSONStream from 'JSONStream';
+import { join } from 'path';
+import { Transform } from 'stream';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { GetMoviesDto } from './dto/get-movies.dto';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
@@ -12,12 +18,51 @@ export class MoviesController {
   }
 
   @Get()
-  findAll() {
-    return this.moviesService.findAll();
-  }
+  findAll(@Query() filter: GetMoviesDto, @Res() res: Response) {
+    // TODO Hide in env
+    const readStream = createReadStream(
+      join(process.cwd(), 'src', 'db', 'db.json'),
+      'utf-8',
+    );
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.moviesService.findOne(+id);
+    const durationTransformer = new Transform({
+      objectMode: true,
+      transform(jsonItem, encoding, callback): void {
+        const runtime = Number(jsonItem.runtime);
+        if (runtime >= filter.duration - 10 && runtime <= filter.duration) {
+          callback(null, jsonItem);
+        } else {
+          callback(); // Dont push chunk
+        }
+      },
+    });
+
+    const filteredMovies = [];
+
+    const genreTransformer = new Transform({
+      objectMode: true,
+      transform(jsonItem, encoding, callback): void {
+        const intersection = filter.genres.filter((element) =>
+          jsonItem.genres.includes(element),
+        ).length;
+
+        if (intersection) {
+          filteredMovies.push({ ...jsonItem, intersection });
+        }
+        callback();
+      },
+    });
+
+    readStream
+      .pipe(JSONStream.parse('movies.*')) // TODO remove hardcoded movies
+      .pipe(durationTransformer) // TODO Make one tranformer module
+      .pipe(genreTransformer)
+      .pipe(JSONStream.stringify()) // TODO Convert it back to JSON
+      .on('end', () => {
+        const sortedMovies = filteredMovies.sort(
+          (a, b) => b.intersection - a.intersection,
+        );
+        res.send(sortedMovies); // TODO Implement mapper to remove intersection property
+      });
   }
 }
