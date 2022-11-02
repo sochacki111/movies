67b75e2545e468a8bb8fc18dc5b2e887c5a5c63e
sochacki111 <michal.sochacki111@gmail.com>
2022-11-02 13:38:50 +0100

    feat: check db file exists on app bootstrap

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import fs from 'fs';
+import { AppConfigService } from './common/config/app-config.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnApplicationBootstrap {
+  constructor(private readonly configService: AppConfigService) {}
+
+  // TODO change to healthz
   getHello(): string {
     return 'Hello World!';
+  }
+
+  onApplicationBootstrap(): void {
+    // Intentional use of sync blocking code to check if db file exist on app bootstrap
+    if (!fs.existsSync(this.configService.get('dbJsonPath'))) {
+      throw new Error('Database file not found');
+    }
   }
 }
